b44a3eaac5e054971b7e46b3d22c2e13edd107a5
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-20 13:49:04 +0300

    install react tailwind css

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-indigo-700 border-b border-indigo-500'>
+        <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
+            <div className='flex h-2 items-center justify-between md:items-stretch md:justify-start'>
+                <a className='flex flex-1 items-center mr-4' href='/index.html'>
+                </a>
+                
+            </div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
