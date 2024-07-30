8a5a0d82dfc6157d277abb61c71234089c0e7f4c
Leah Nyambura <leahnyambura682@gmail.com>
2024-07-30 14:19:54 +0300

    introduction of the jobs.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
+import JobListings from "./components/JobListings"
 import Navbar from "./components/Navbar"
 
 
@@ -11,6 +12,7 @@ function App() {
     <Navbar/>
     <Hero/>
     <HomeCards/>
+    <JobListings/>
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,9 @@
-const Card = ({ children, bg = 'bg-gray-100' }) => {
-    return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
-  };
-  export default Card;
+import React from 'react'
+
+const Card = ({children }) => {
+  return (
+   <div className='bg-gray-100 p-6 rounded-lg shadow-md'>{children}</div>
+  )
+}
+
+export default Card
